94e193ced2e351f0c136d79ccbb4dd6d5e902ff8
Negi47 <narendra.negi47@gmail.com>
2018-12-26 08:08:44 +0530

    login code started

---
+++ js/login.js
@@ -0,0 +1,7 @@
+function login(){
+    console.log("working")
+    // var user = document.getElementById('user');
+    // var pswd = document.getElementById('pswd');
+    // console.log(user+" "+pswd);
+    return false;
+}
